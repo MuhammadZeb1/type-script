5149c8729d48454b365822305249e7ac864c1e03
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-04-06 10:54:38 +0500

    how to write the object in the ts

---
+++ L2/app.js
@@ -0,0 +1,7 @@
+// object , array , tuple , enum , any , void , never , unknown
+// object 
+var person = {
+    name: "khan ",
+    address: 12
+};
+console.log(person.name);
---
+++ L2/app.ts
@@ -0,0 +1,13 @@
+// object , array , tuple , enum , any , void , never , unknown
+
+// object 
+
+const person: {
+    name :string;
+    address: number
+}={
+    name:"khan ",
+    address:12
+}
+
+console.log(person.name)
