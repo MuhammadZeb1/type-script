49e21b37af6a2f0f49916c26543caae2820f8b36
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-04-14 12:45:00 +0500

    i lerarn about the literal in the ts

--- L3/app.js
+++ L3/app.js
@@ -2,4 +2,25 @@
 // union type
 var person = "ali khan ";
 person = 12;
-console.log(person);
+// console.log(person)  
+// function add (num:number | string, num1:number | string){
+//     if (typeof num === "number" && typeof num1 === "number") {
+//         console.log(num+num1)  
+//     } else {
+//         console.log("both parameters should be numbers")
+//     }
+// }
+// add("23",4)
+function add(num, num1, conversion) {
+    if (conversion === "as-number") {
+        console.log(+num + +num1);
+    }
+    else if (conversion === "as-string") {
+        console.log(num.toString() + num1.toString());
+    }
+    else {
+        console.log("both parameters should be numbers");
+    }
+}
+add("23", 4, "as-number");
+add("ali", " khan", "as-string");
--- L3/app.ts
+++ L3/app.ts
@@ -5,13 +5,29 @@
 
 let person  : string | number = "ali khan "
 person = 12
-console.log(person)  
+// console.log(person)  
 
-function add (num:number | string, num1:number | string){
-    if (typeof num === "number" && typeof num1 === "number") {
-        console.log(num+num1)  
-    } else {
+// function add (num:number | string, num1:number | string){
+//     if (typeof num === "number" && typeof num1 === "number") {
+//         console.log(num+num1)  
+//     } else {
+//         console.log("both parameters should be numbers")
+//     }
+// }
+// add("23",4)
+
+
+// literal type
+function add (num:number | string, num1:number | string, conversion: "as-number" | "as-string"){
+    if (conversion === "as-number") {
+        console.log(+num+ +num1)  
+    } 
+    else if (conversion === "as-string") {
+        console.log(num.toString() + num1.toString())  
+    }
+    else {
         console.log("both parameters should be numbers")
     }
 }
-add("23",4)
+add("23",4,"as-number")
+add("ali"," khan","as-string")
