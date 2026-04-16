b869127a7a24660b862275e99ef0a7b7e322b445
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-04-16 19:16:55 +0500

    learn the the function and it's return type

---
+++ L4/app.js
@@ -0,0 +1,8 @@
+// function return type 
+// function functioNName(): returnType {
+//     console.log("this is a function with void return type")
+// }
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(23, 4));
---
+++ L4/app.ts
@@ -0,0 +1,14 @@
+// function return type 
+
+// function functioNName(): returnType {
+//     console.log("this is a function with void return type")
+// }
+
+function add(num1:number, num2:number): number {
+    return num1 + num2
+}   
+console.log(add(23,4))
+
+// we can use the ( string, number , boolean , void , never , unknown ) as return type of function
+
+// void return type
