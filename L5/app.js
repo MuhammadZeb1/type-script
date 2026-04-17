0d15393c2b91de1870fed24991004926e125fdb8
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-04-17 11:18:14 +0500

    lerant about about the unknow and any

---
+++ L5/app.js
@@ -0,0 +1,5 @@
+//  any , unknown , never , void
+var a = 10;
+var b = a;
+console.log(typeof a);
+console.log(typeof b);
---
+++ L5/app.ts
@@ -0,0 +1,14 @@
+//  any , unknown , never , void
+
+// any → TypeScript blind ho jata hai
+// let a : any = 10
+
+// unknown → TypeScript careful ho jata hai 🔍
+// let a : unknown = 10
+
+
+let b : string= a;
+
+
+console.log(typeof a);
+console.log(typeof b);
